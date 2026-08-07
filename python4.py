236ca0be23964cf5494eb7d07dd71d1147c32db8
amitjapulkar10 <amitjapulkar10@gmail.com>
2026-08-07 20:30:09 +0530

    Initial commit

---
+++ python4.py
@@ -0,0 +1,10 @@
+# List of numbers
+data = [45, 12, 89, 2, 67]
+
+# Find largest and smallest number
+largest = max(data)
+smallest = min(data)
+
+# Display results
+print("Largest number:", largest)
+print("Smallest number:", smallest)
