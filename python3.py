6a5bbadf31696990579c18c456156e53f9385468
amitjapulkar10 <amitjapulkar10@gmail.com>
2026-08-07 20:11:07 +0530

    its me

---
+++ python3.py
@@ -0,0 +1,12 @@
+# List of numbers
+numbers = [10, 20, 30, 40, 50]
+
+# Find total sum
+total = sum(numbers)
+
+# Find average
+average = total / len(numbers)
+
+# Display results
+print("Total Sum:", total)
+print("Average:", average)
