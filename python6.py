ac3a3e82901aace2ebc0186e7132bba94de90385
amitjapulkar10 <amitjapulkar10@gmail.com>
2026-08-10 21:13:19 +0530

    Its me

---
+++ python6.py
@@ -0,0 +1,13 @@
+fruits = {"apple", "banana", "cherry"}
+
+# Add element
+fruits.add("mango")
+print(fruits)
+
+# Remove element
+fruits.remove("banana")
+print(fruits)
+
+# Discard element
+fruits.discard("apple")
+print(fruits)
