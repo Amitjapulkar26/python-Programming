2aa3234f2258f7b2ef93bda4970505bc7635547e
amitjapulkar10 <amitjapulkar10@gmail.com>
2026-08-10 21:25:56 +0530

    Initial Commit

---
+++ python11.py
@@ -0,0 +1,16 @@
+student = {
+    "name": "Alice",
+    "age": 20,
+    "grade": "B"
+}
+
+# Add new key-value pair
+student["city"] = "Nagpur"
+
+# Modify existing value
+student["age"] = 21
+
+# Access a key
+print("Name:", student["name"])
+
+print(student)
